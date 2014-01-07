652ec8d856e28d6a0d33b4b61ecd2fc0447cf4d3
PeterDobbe <p.b.dobbe@student.tudelft.nl>
2014-01-07 11:00:33 +0100

    Teken klasse is ook een stuk korter na het toevoegen van drawPlane methode

--- MazeRunner/src/menu/Teken.java
+++ MazeRunner/src/menu/Teken.java
@@ -127,35 +127,8 @@ public class Teken {
 		gl.glVertex2f(x2, y2);
 		gl.glEnd();
 	}
-
-	public static void drawCuboid(GL gl, double xmin, double xmax, double ymin,
-			double ymax, double zmin, double zmax, int tex) {
-		drawCuboid(gl, xmin, xmax, ymin, ymax, zmin, zmax, new int[] { tex,
-				tex, tex, tex, tex, tex });
-	}
-
-	public static void drawCuboid(GL gl, double xmin, double xmax, double ymin,
-			double ymax, double zmin, double zmax, int[] texList) {
-		Vertex v1 = new Vertex(xmin, ymin, zmin);
-		Vertex v2 = new Vertex(xmin, ymin, zmax);
-		Vertex v3 = new Vertex(xmax, ymin, zmax);
-		Vertex v4 = new Vertex(xmax, ymin, zmin);
-		Vertex v5 = new Vertex(xmin, ymax, zmin);
-		Vertex v6 = new Vertex(xmin, ymax, zmax);
-		Vertex v7 = new Vertex(xmax, ymax, zmax);
-		Vertex v8 = new Vertex(xmax, ymax, zmin);
-		Vector n1 = Vector.calcNormal(v1, v2, v3); // Normal floor plane
-		Vector n2 = Vector.calcNormal(v5, v6, v7); // Normal top plane
-		Vector n3 = Vector.calcNormal(v4, v8, v7); // Normal back plane
-		Vector n4 = Vector.calcNormal(v3, v7, v6); // Normal right plane
-		Vector n5 = Vector.calcNormal(v6, v2, v1); // Normal front plane
-		Vector n6 = Vector.calcNormal(v5, v1, v4); // Normal left plane
-		
-		gl.glDisable(GL.GL_CULL_FACE);
-
-		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[0]);
+	public static void drawQuad(GL gl, Vertex v1, Vertex v2, Vertex v3, Vertex v4, Vector n1){
 		gl.glBegin(GL.GL_QUADS);
-		// Floor plane
 		gl.glNormal3d(n1.x,n1.y,n1.z);
 		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(v1.x, v1.y, v1.z);
@@ -166,76 +139,54 @@ public class Teken {
 		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(v4.x, v4.y, v4.z);
 		gl.glEnd();
-
-		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[1]);
-		gl.glBegin(GL.GL_QUADS);
+	}
+	public static void drawQuad(GL gl, Vertex v1, Vertex v2, Vertex v3, Vertex v4){
+		Vector n1 = Vector.calcNormal(v1, v2, v3);
+		drawQuad(gl, v1, v2, v3, v4, n1);
+	}
+	public static void drawCuboid(GL gl, double xmin, double xmax, double ymin,
+			double ymax, double zmin, double zmax, int tex) {
+		drawCuboid(gl, xmin, xmax, ymin, ymax, zmin, zmax, new int[] { tex,
+				tex, tex, tex, tex, tex });
+	}
+	
+	public static void drawCuboid(GL gl, double xmin, double xmax, double ymin,
+			double ymax, double zmin, double zmax, int[] texList) {
+		Vertex v1 = new Vertex(xmin, ymin, zmin);
+		Vertex v2 = new Vertex(xmin, ymin, zmax);
+		Vertex v3 = new Vertex(xmax, ymin, zmax);
+		Vertex v4 = new Vertex(xmax, ymin, zmin);
+		Vertex v5 = new Vertex(xmin, ymax, zmin);
+		Vertex v6 = new Vertex(xmin, ymax, zmax);
+		Vertex v7 = new Vertex(xmax, ymax, zmax);
+		Vertex v8 = new Vertex(xmax, ymax, zmin);
+		
+		gl.glDisable(GL.GL_CULL_FACE);
+		
+		// Floor plane
+		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[0]);
+		drawQuad(gl, v1, v2, v3, v4);
+		
 		// Top plane
-		gl.glNormal3d(n2.x, n2.y, n2.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v5.x, v5.y, v5.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v6.x, v6.y, v6.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v7.x, v7.y, v7.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v8.x, v8.y, v8.z);
-		gl.glEnd();
-
-		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[2]);
-		gl.glBegin(GL.GL_QUADS);
+		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[1]);
+		drawQuad(gl, v5, v6, v7, v8);
+			
 		// Back plane
-		gl.glNormal3d(n3.x, n3.y, n3.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v4.x, v4.y, v4.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v8.x, v8.y, v8.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v7.x, v7.y, v7.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v3.x, v3.y, v3.z);
-		gl.glEnd();
-		
-		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[3]);
-		gl.glBegin(GL.GL_QUADS);
+		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[2]);
+		drawQuad(gl, v4, v8, v7, v3);
+				
 		// Right side plane
-		gl.glNormal3d(n4.x, n4.y, n4.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v3.x, v3.y, v3.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v7.x, v7.y, v7.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v6.x, v6.y, v6.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v2.x, v2.y, v2.z);
-		gl.glEnd();
+		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[3]);
+		drawQuad(gl, v3, v7, v6, v2);
 		
-		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[4]);
-		gl.glBegin(GL.GL_QUADS);
 		// Front plane
-		gl.glNormal3d(n5.x, n5.y, n5.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v6.x, v6.y, v6.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v2.x, v2.y, v2.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v1.x, v1.y, v1.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v5.x, v5.y, v5.z);
-		gl.glEnd();
+		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[4]);
+		drawQuad(gl, v6, v2, v1, v5);
 		
-		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[5]);
-		gl.glBegin(GL.GL_QUADS);
 		// Left plane
-		gl.glNormal3d(n6.x, n6.y, n6.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v5.x, v5.y, v5.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v1.x, v1.y, v1.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v4.x, v4.y, v4.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v8.x, v8.y, v8.z);
-		gl.glEnd();
+		gl.glBindTexture(GL.GL_TEXTURE_2D, texList[5]);
+		drawQuad(gl, v5, v1, v4, v8);
+		
 		gl.glEnable(GL.GL_CULL_FACE);
 	}
 	public static void drawCuboid(GL gl, double xmin, double xmax, double ymin,
@@ -248,92 +199,15 @@ public class Teken {
 		Vertex v6 = new Vertex(xmin, ymax, zmax);
 		Vertex v7 = new Vertex(xmax, ymax, zmax);
 		Vertex v8 = new Vertex(xmax, ymax, zmin);
-		Vector n1 = Vector.calcNormal(v1, v2, v3); // Normal floor plane
-		Vector n2 = Vector.calcNormal(v5, v6, v7); // Normal top plane
-		Vector n3 = Vector.calcNormal(v4, v8, v7); // Normal back plane
-		Vector n4 = Vector.calcNormal(v3, v7, v6); // Normal right plane
-		Vector n5 = Vector.calcNormal(v6, v2, v1); // Normal front plane
-		Vector n6 = Vector.calcNormal(v5, v1, v4); // Normal left plane
 		
 		gl.glDisable(GL.GL_CULL_FACE);
 
-		gl.glBegin(GL.GL_QUADS);
-		// Floor plane
-		gl.glNormal3d(n1.x,n1.y,n1.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v1.x, v1.y, v1.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v2.x, v2.y, v2.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v3.x, v3.y, v3.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v4.x, v4.y, v4.z);
-		gl.glEnd();
-
-		gl.glBegin(GL.GL_QUADS);
-		// Top plane
-		gl.glNormal3d(n2.x, n2.y, n2.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v5.x, v5.y, v5.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v6.x, v6.y, v6.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v7.x, v7.y, v7.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v8.x, v8.y, v8.z);
-		gl.glEnd();
-
-		gl.glBegin(GL.GL_QUADS);
-		// Back plane
-		gl.glNormal3d(n3.x, n3.y, n3.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v4.x, v4.y, v4.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v8.x, v8.y, v8.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v7.x, v7.y, v7.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v3.x, v3.y, v3.z);
-		gl.glEnd();
-		
-		gl.glBegin(GL.GL_QUADS);
-		// Right side plane
-		gl.glNormal3d(n4.x, n4.y, n4.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v3.x, v3.y, v3.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v7.x, v7.y, v7.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v6.x, v6.y, v6.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v2.x, v2.y, v2.z);
-		gl.glEnd();
-		
-		gl.glBegin(GL.GL_QUADS);
-		// Front plane
-		gl.glNormal3d(n5.x, n5.y, n5.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v6.x, v6.y, v6.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v2.x, v2.y, v2.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v1.x, v1.y, v1.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v5.x, v5.y, v5.z);
-		gl.glEnd();
-		
-		gl.glBegin(GL.GL_QUADS);
-		// Left plane
-		gl.glNormal3d(n6.x, n6.y, n6.z);
-		gl.glTexCoord2d(1.0, 1.0);
-		gl.glVertex3d(v5.x, v5.y, v5.z);
-		gl.glTexCoord2d(1.0, 0.0);
-		gl.glVertex3d(v1.x, v1.y, v1.z);
-		gl.glTexCoord2d(0.0, 0.0);
-		gl.glVertex3d(v4.x, v4.y, v4.z);
-		gl.glTexCoord2d(0.0, 1.0);
-		gl.glVertex3d(v8.x, v8.y, v8.z);
-		gl.glEnd();
+		drawQuad(gl, v1, v2, v3, v4);		// Floor plane
+		drawQuad(gl, v5, v6, v7, v8);		// Top plane
+		drawQuad(gl, v4, v8, v7, v3);		// Back plane
+		drawQuad(gl, v3, v7, v6, v2);		// Right plane
+		drawQuad(gl, v6, v2, v1, v5);		// Front plane
+		drawQuad(gl, v5, v1, v4, v8);		// Left plane
 		
 		gl.glEnable(GL.GL_CULL_FACE);
 	}
