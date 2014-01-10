f42170f902bc67c60770e0f2defedd965378cbd4
PeterDobbe <p.b.dobbe@student.tudelft.nl>
2014-01-10 15:45:29 +0100

    "I'm every woman, it's all you need!" ofsoz. Bullet can now hit player. Sound loading is improved

--- MazeRunner/src/engine/MazeRunner.java
+++ MazeRunner/src/engine/MazeRunner.java
@@ -737,6 +737,12 @@ public class MazeRunner {
 					break;
 				}
 			}
+			if (b.touches(player)){
+				player.score+=2001;
+				PlayerState.getState(MazeRunner.player.playerStateInt).leaving();
+				MazeRunner.player.playerStateInt = 3;
+				PlayerState.getState(MazeRunner.player.playerStateInt).entering();
+			}
 
 		}
 		for (int rNr = 0; rNr < roofList.size(); rNr++) {
@@ -746,7 +752,7 @@ public class MazeRunner {
 				r.fallingSpeed = r.fallingSpeed * 1.005;
 				Maze maze = level.getMaze(r.mazeID);
 				if (r.locationY < maze.mazeY + 0.5) {
-					Sound.roofCrash.play();
+					Sound.sounds.get("roofCrash").play();
 					roofList.remove(r);
 					r.setLegal(false);
 					for (int eNr = 0; eNr < enemyList.size(); eNr++) {
